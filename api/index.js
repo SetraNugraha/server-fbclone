61ef606d4c96a0a431e31be9561720014fef8178
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-08-21 18:23:51 +0700

    remove app.listen

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import router from '../routes/index.js'
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT
 
 app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
 app.use(cookieParser())
@@ -15,6 +14,3 @@ app.use(router)
 app.use('/img/post_images', express.static('public/img/post_images'))
 app.use('/img/profile_images', express.static('public/img/profile_images'))
 
-app.listen(PORT, () => {
-  console.log(`Server Running on PORT: ${PORT}`)
-})
