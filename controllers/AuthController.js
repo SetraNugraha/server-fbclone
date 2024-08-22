1e8cf6b40fc610c7f147cdcb9d1870ea6439c365
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-08-22 17:08:03 +0700

    add secure in res.cookie

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -76,6 +76,7 @@ const login = async (req, res) => {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000, // Hour * minute * second * mili second = 1 Day
     })
 
