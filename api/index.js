ae90d31c79de601bd880868cf92ec979dfdeb0b3
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-08-23 17:53:05 +0700

    update cors

--- api/index.js
+++ api/index.js
@@ -10,6 +10,8 @@ const app = express()
 app.use(
   cors({
     origin: 'https://client-fbclone.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }),
 )
