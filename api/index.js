d3f6ca6ac5491d65624d34b632b918d7b2b645a4
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-08-24 17:23:27 +0700

    add localhost cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: 'https://client-fbclone.vercel.app',
+    origin: ['https://client-fbclone.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
